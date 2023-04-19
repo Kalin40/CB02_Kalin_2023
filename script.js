dce43e025105da7d26b511abed60ecc03cfd989b
Kalin <k.kanev@student.fontys.nl>
2023-04-19 21:15:37 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const darkModeBtn = document.getElementById("dark-mode-btn");
+const darkMode = localStorage.getItem("darkMode");
 const toggleIcon = document.querySelector(".toggle-icon");
 const burgerMenu = document.querySelector('.burger-menu');
 const navLinks = document.querySelector('nav ul');
 const navLinksLi = document.querySelectorAll('nav ul li');
 
+/*--------------------------------------------------------------------------*/
+
+//Burger menu for smaller screens
 burgerMenu.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 
@@ -19,12 +23,38 @@ burgerMenu.addEventListener('click', () => {
     // Toggle the burger menu animation
     burgerMenu.classList.toggle('toggle');
 });
+/*--------------------------------------------------------------------------*/
 
 
 darkModeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
-    toggleIcon.classList.toggle("active");
+
+    console.log("body.classList", document.body.classList); // Verify that the dark mode class is toggled on and off
+
+    const toggleIcon = document.getElementById("toggle-icon"); // Retrieve the toggle-icon element inside the click event listener
+
+    // Update the icon's content
+    if (document.body.classList.contains("dark-mode")) {
+        toggleIcon.textContent = "☀️";
+        ; // Use escape sequence for sun icon
+    } else {
+        toggleIcon.textContent = "🌙"; // Use escape sequence for moon icon
+    }
 });
+
+/*--------------------------------------------------------------------------*/
+
+//Scrool to the top of the page
+const scrollToTopBtn = document.getElementById("scroll-to-top");
+scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
+
+/*--------------------------------------------------------------------------*/
 
 
 document.querySelector('.clickable-sectionUCD').addEventListener('click', function () {
@@ -42,3 +72,5 @@ document.querySelector('.clickable-sectionCode').addEventListener('click', funct
 document.querySelector('.clickable-sectionProject').addEventListener('click', function () {
     window.location.href = 'Project.html';
 });
+
+/*--------------------------------------------------------------------------*/
